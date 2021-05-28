ffa8f141aaf9b6734e9a18b6944308951a277f09
Thành Vinh <you@example.com>
2021-05-28 14:30:48 +0700

    add Cau2_GetData.txt

--- app/src/main/java/com/example/lenguyenthanhvinh_18053681_thithu/Mock_API.java
+++ app/src/main/java/com/example/lenguyenthanhvinh_18053681_thithu/Mock_API.java
@@ -3,12 +3,213 @@ package com.example.lenguyenthanhvinh_18053681_thithu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Mock_API extends AppCompatActivity {
-
+    String url ="https://60ada5b880a61f0017331630.mockapi.io/Students";
+    ListView listDanhSach;
+    Button btnThem, btnSua;
+    ArrayAdapter<String> arrayAdapter;
+    ArrayList<String> arrayList;
+    TextView tvDisplay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mock__a_p_i);
+        listDanhSach = findViewById(R.id.lstDanhSach);
+        btnThem = findViewById(R.id.btnThem);
+        btnSua = findViewById((R.id.btnSua));
+        tvDisplay = findViewById(R.id.txtTen);
+        arrayList = new ArrayList<String>();
+        //GetData(url);
+        GetArrayJson(url);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1 , arrayList);
+        listDanhSach.setAdapter(arrayAdapter);
+        btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PostApi(url);
+            }
+        });
+        DeleteApi(url);
+
     }
+
+    private void GetData(String url){
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,
+                url, new com.android.volley.Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+               // tvDisplay.setText(response.toString());
+            }
+        }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Mock_API.this,
+                        "Error make by API server!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private void GetJson(String url){
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET, url, null,
+                new com.android.volley.Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+//                        try {
+//                            arrayList.add(response.toString());
+//                            tvDisplay.setText(
+//                                    response.getString("name").toString());
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
+
+                    }
+                },
+                new com.android.volley.Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Mock_API.this, "Error by get JsonObject...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonObjectRequest);
+
+    }
+
+    private void GetArrayJson(String url){
+        JsonArrayRequest jsonArrayRequest =
+                new JsonArrayRequest(url,
+                        new com.android.volley.Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray response) {
+                                for(int i=0; i<response.length(); i++){
+                                    try {
+                                        String ten="";
+                                        int tuoi = 0;
+                                        JSONObject object = (JSONObject) response.get(i);
+                                        arrayList.add(((JSONObject) response.get(i)).toString());
+
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        }, new com.android.volley.Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Mock_API.this, "Error by get Json Array!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    private void PostApi(String url){
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                url,
+                new com.android.volley.Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(Mock_API.this, "Successfully", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new com.android.volley.Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Mock_API.this, "Error by Post data!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                HashMap<String, String>
+                        params = new HashMap<>();
+                params.put("id","17");
+                params.put("name", "Lâm");
+                params.put("age", "40");
+
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private void PutApi(String url){
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.PUT, url + '/' + 28, new com.android.volley.Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(Mock_API.this, "Successfully", Toast.LENGTH_SHORT).show();
+            }
+        }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Mock_API.this, "Error by Post data!", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> params = new HashMap<>();
+
+                params.put("name", "Lâm");
+                params.put("age", "30");
+
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+    private void DeleteApi(String url){
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.DELETE, url + '/' + 37, new com.android.volley.Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(Mock_API.this, "Successfully", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Mock_API.this, "Error by Post data!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
 }
