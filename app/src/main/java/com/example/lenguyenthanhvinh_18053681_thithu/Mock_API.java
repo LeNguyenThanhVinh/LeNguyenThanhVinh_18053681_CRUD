c36521f8ecea938a309436761178e22f811f6654
Thành Vinh <you@example.com>
2021-05-28 13:33:51 +0700

    add DangNhap.txt

---
+++ app/src/main/java/com/example/lenguyenthanhvinh_18053681_thithu/Mock_API.java
@@ -0,0 +1,14 @@
+package com.example.lenguyenthanhvinh_18053681_thithu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mock_API extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mock__a_p_i);
+    }
+}
